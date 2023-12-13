2b0e717fa85ed1b2c2101c318343e1852eeec89b
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-12-13 14:34:42 -0300

    first commit section technologies

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import About from './About/About';
+import Technologies from './Technologies/Technologies';
 
 function Home() {
 
   return (
     <>
       <About />
+      <Technologies />
     </>
   )
 }
