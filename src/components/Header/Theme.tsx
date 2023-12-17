6eb1dafb9ee7b1ccbbc09b85d7b90ca48c16eda1
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-12-17 15:14:20 -0300

    save theme at sessionStorage

--- src/components/Header/Theme.tsx
+++ src/components/Header/Theme.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import IconThemeLight from "../../assets/svg/IconThemeLight";
 import IconThemeDark from "../../assets/svg/IconThemeDark";
 import styles from "./Header.module.css";
+import { getTheme, saveTheme } from "../../store/theme";
+
 
 function Theme() {
-  const [theme, setTheme] = React.useState(true); //true= darkTheme, false=lightTheme
+  const [theme, setTheme] = React.useState(getTheme() ?? true); //true= darkTheme, false=lightTheme
 
   React.useEffect(() => {
-    (theme) ? 
+    if(theme) {
       document.querySelector('html')?.setAttribute('class', 'dark')
-    :
-    document.querySelector('html')?.setAttribute('class', 'light')
+      saveTheme(true)
+    } else {
+      document.querySelector('html')?.setAttribute('class', 'light')
+      saveTheme(false)
+    }
   }, [theme])
 
   return (
