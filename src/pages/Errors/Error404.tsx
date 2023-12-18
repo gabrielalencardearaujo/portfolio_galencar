6d345701b9e6cd1966b017479e9d458217dc04e7
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-12-18 18:52:35 -0300

    create page 404 not found

--- src/pages/Errors/Error404.tsx
+++ src/pages/Errors/Error404.tsx
@@ -1,17 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import styles from './Error404.module.css';
+import rickMorty from '../../assets/img/rick_morty.png'
 
 function Error404() {
-  const navigate = useNavigate();
-
-  function handleClick() {
-    navigate('/');
-  }
-
   return (
-    <>
-      <h1>Pagina nao encontrada</h1>
-      <button onClick={handleClick}>ir para a pagina Home</button>
-    </>
+    <section className={styles.container}>
+      <span>
+        <h1 className={styles.title}>404</h1>
+        <p className={styles.notFound}>Página não encontrada</p>
+        <p className={styles.sub} title="">Porque este erro aconteceu?</p>
+      </span>
+      <img src={rickMorty} alt="Rick and Morty" />
+    </section>
   );
 }
 
