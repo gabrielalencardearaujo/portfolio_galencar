00fac2873c2b954507f8cd904c00723836eeb35d
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-12-20 16:12:48 -0300

    alteracao da imagem quizFlag

--- src/assets/img/utils.ts
+++ src/assets/img/utils.ts
@@ -1,12 +1,15 @@
 import AppDog from './Projects/AppDog.jpg';
 import ProjetoEspelho from './Projects/projetoEspelho.png';
+import Protfolio from './Projects/portfolio.png';
+import RegexTool from './Projects/regexTool.png';
+import QuizFlag from './Projects/quizFlag.png';
 
 export const describeCardProjects = [
   {
-    name: 'AppDog',
-    describe: 'Projeto desenvolvido nas aulas de React do Curso de React do Origamid, o site é uma rede social para cachorros e usando tecnologias como React.JSX, HTML, CSS, JS e Vite.',
-    src: AppDog,
-    github: 'https://github.com/gabrielalencardearaujo/app_dog_social_media',
+    name: 'Portfolio',
+    describe: 'Portfolio com meus objetivos, projetos e estudos. Desenvolvido usando HTML, CSS, Typescript, React.TSX',
+    src: Protfolio,
+    github: 'https://github.com/gabrielalencardearaujo/portfolio_galencar',
     website: ''
   },
   {
@@ -17,24 +20,24 @@ export const describeCardProjects = [
     website: ''
   },
   {
-    name: 'Projeto Espelho',
-    describe: 'Desenvolvido para ajudar e facilitar a reversa (conferência) dos terminais de máquinas de cartão de crédito no meu local de trabalho e usando tecnologias como HTML5, CSS3, JS e JQquery.',
-    src: ProjetoEspelho,
-    github: 'https://github.com/gabrielalencardearaujo/Projeto_Espelho',
+    name: 'AppDog',
+    describe: 'Projeto desenvolvido nas aulas de React do Curso de React do Origamid, o site é uma rede social para cachorros e usando tecnologias como React.JSX, HTML, CSS, JS e Vite.',
+    src: AppDog,
+    github: 'https://github.com/gabrielalencardearaujo/app_dog_social_media',
     website: ''
   },
   {
-    name: 'Projeto Espelho',
-    describe: 'Desenvolvido para ajudar e facilitar a reversa (conferência) dos terminais de máquinas de cartão de crédito no meu local de trabalho e usando tecnologias como HTML5, CSS3, JS e JQquery.',
-    src: ProjetoEspelho,
-    github: 'https://github.com/gabrielalencardearaujo/Projeto_Espelho',
+    name: 'RegexTool',
+    describe: 'WebApp para estudar Regex, com feedback dinâmico e instantâneo dos caracteres selecionados usando expressões regulares. Projeto desenvolvido com HTML, CSS, e Typescript.',
+    src: RegexTool,
+    github: 'https://github.com/gabrielalencardearaujo/Character_Counter',
     website: ''
   },
   {
-    name: 'Projeto Espelho',
-    describe: 'Desenvolvido para ajudar e facilitar a reversa (conferência) dos terminais de máquinas de cartão de crédito no meu local de trabalho e usando tecnologias como HTML5, CSS3, JS e JQquery.',
-    src: ProjetoEspelho,
-    github: 'https://github.com/gabrielalencardearaujo/Projeto_Espelho',
+    name: 'QuizFlag',
+    describe: 'Mini Jogo para melhorar seus conhecimentos sobre bandeiras de países. Jogo conta com sistema de vidas, temporizador e placar. Tecnologias usads: HTML, CSS e Javascript',
+    src: QuizFlag,
+    github: 'https://github.com/gabrielalencardearaujo/game_quiz_flags',
     website: ''
   },
   {
