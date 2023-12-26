b54d06a5b89cc884bfe1de5842cbf7b49e90246a
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-12-26 17:39:47 -0300

    add pds certificados

--- src/services/jsonFormatCardCertificate.ts
+++ src/services/jsonFormatCardCertificate.ts
@@ -1,3 +1,4 @@
+//Imports IMG:
 import logoPUCPR from '../assets/img/Certificates/logo-PUCPR.jpg';
 import logoUFPE from '../assets/img/Certificates/Logo-ufpe-2-2.jpg';
 import certificadoWebCompleto from '../assets/img/Certificates/certificadoWebCompleto.png';
@@ -11,8 +12,19 @@ import CSSFlexbox from '../assets/img/Certificates/CSS_Flexbox.png';
 import SASS from '../assets/img/Certificates/CSS_SASS.png';
 import UiDesign from '../assets/img/Certificates/UI_Design_Iniciantes.png';
 import JQuery from '../assets/img/Certificates/JQuery.png';
-import PDF_certificadoWebCompleto from '../assets/img/Certificates/PDFs/DesenvolvimentoWeb_Udemy.pdf';
 
+// Imports PDFs:
+import PDF_certificadoWebCompleto from '../assets/img/Certificates/PDFs/DesenvolvimentoWeb_Udemy.pdf';
+import CertificadoFlexBox from '../assets/img/Certificates/PDFs/certificado-flexbox.pdf';
+import CertificadoCSSAvancado from '../assets/img/Certificates/PDFs/certificado-css-avancado.pdf';
+import CertificadoGridLayout from '../assets/img/Certificates/PDFs/certificado-grid-layout.pdf';
+import CertificadoTypescript from '../assets/img/Certificates/PDFs/certificado-Typescript.pdf';
+import CertificadoUIDesign from '../assets/img/Certificates/PDFs/certificado-UI-Design.pdf';
+import CertificadoJQuery from '../assets/img/Certificates/PDFs/certificado-jQuery.pdf';
+import CertificadoSass from '../assets/img/Certificates/PDFs/certificado-sass.pdf';
+import CertificadoReact from '../assets/img/Certificates/PDFs/certificado-react.pdf';
+import CertificadoReactComTypescript from '../assets/img/Certificates/PDFs/certificado-reactTSX.pdf';
+import CertificadoGITGITHUB from '../assets/img/Certificates/PDFs/Gabriel-Alencar-de-Araujo-Git-e-GitHub-20-Horas-Certificado-Curso-em-Video.pdf';
 
 export type TypeJsonFormation = {
   src: string,
@@ -44,60 +56,70 @@ export const jsonFormatCardCertificate: TypeJsonFormation[] = [
   },
   {
     src: GitGithubGuanabara,
+    pdf: CertificadoGITGITHUB,
     describe: "Curso Online de Git/Github no Curso em Video - 20 horas",
     dateStart: "Ago/2023",
     dateEnd: "Ago/2023",
   },
   {
     src: UiDesign,
+    pdf: CertificadoUIDesign,
     describe: "Curso Online de UI Design para Iniciantes na Origamid - 34 horas",
     dateStart: "Jul/2023",
     dateEnd: "Ago/2023",
   },
   {
     src: JQuery,
+    pdf: CertificadoJQuery,
     describe: "Curso Online de JQuery na Origamid - 18 horas",
     dateStart: "Ago/2023",
     dateEnd: "Ago/2023",
   },
   {
     src: ReactCompleto,
+    pdf: CertificadoReact,
     describe: "Curso Online de React.JSX na Origamid - 36 horas",
     dateStart: "Out/2023",
     dateEnd: "Out/2023",
   },
   {
     src: ReactTypescript,
+    pdf: CertificadoReactComTypescript,
     describe: "Curso Online de React com Typescript na Origamid - 10 horas",
     dateStart: "Nov/2023",
     dateEnd: "Nov/2023",
   },
   {
     src: TypescriptIniciantes,
+    pdf: CertificadoTypescript,
     describe: "Curso Online de Typescript na Origamid - 22 horas",
     dateStart: "Set/2023",
     dateEnd: "Set/2023",
   },
   {
     src: CSSGridLayout,
+    pdf: CertificadoGridLayout,
     describe: "Curso Online de CSS - Grid Layout na Origamid - 10 horas",
     dateStart: "Ago/2023",
     dateEnd: "Ago/2023",
   },
   {
     src: CSSAvancado,
+    pdf: CertificadoCSSAvancado,
     describe: "Curso Online de CSS Avançado na Origamid - 18 horas",
     dateStart: "Ago/2023",
     dateEnd: "Ago/2023",
   },
   {
     src: CSSFlexbox,
+    pdf: CertificadoFlexBox,
     describe: "Curso Online de CSS Flexbox na Origamid - 6 horas",
     dateStart: "Ago/2023",
     dateEnd: "Ago/2023",
   },
   {
     src: SASS,
+    pdf: CertificadoSass,
     describe: "Curso Online de SASS na Origamid - 12 horas",
     dateStart: "Ago/2023",
     dateEnd: "Ago/2023",
