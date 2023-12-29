196a6c2fa1c073bfd708fe2617028d00b58a0b67
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-12-29 17:16:03 -0300

    responsividade header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import { NavLink, useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
 import Theme from "./Theme";
-import Languages from "./Languages";
+// import Languages from "./Languages";
 import React from "react";
 
 function Header() {
+  const [menuActive, setMenuActive] = React.useState(false);
   const location = useLocation();
 
   React.useEffect(() => {
@@ -31,27 +32,43 @@ function Header() {
           <strong className="active">G.</strong>Alencar
         </NavLink>
       </div>
-      <div>
-        <nav className={styles.navLinks}>
-          <ul>
-            <li>
-              <NavLink to="/" end>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects" end>
-                Projetos
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/education">Formação</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Theme />
-        <Languages />
+
+      <div
+        className={`${styles.mobileMenu} ${
+          menuActive ? styles.menuActive : ""
+        }`}
+        onClick={() => setMenuActive(!menuActive)}
+      >
+        <div className={styles.line1}></div>
+        <div className={styles.line2}></div>
+        <div className={styles.line3}></div>
       </div>
+
+      <nav
+        className={`${styles.navLinks} ${menuActive ? styles.menuActive : ""}`}
+      >
+        <ul>
+          <li onClick={() => setMenuActive(!menuActive)}>
+            <NavLink to="/" end>
+              Home
+            </NavLink>
+          </li>
+          <li onClick={() => setMenuActive(!menuActive)}>
+            <NavLink to="/projects">
+              Projetos
+            </NavLink>
+          </li>
+          <li onClick={() => setMenuActive(!menuActive)}>
+            <NavLink to="/education">Formação</NavLink>
+          </li>
+          <li onClick={() => setMenuActive(!menuActive)}>
+            <Theme />
+          </li>
+          {/* <li onClick={() => setMenuActive(!menuActive)}>
+            <Languages />
+          </li> */}
+        </ul>
+      </nav>
     </header>
   );
 }
